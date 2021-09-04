b80a9c48dfbd21161b13574965ad546707e9a57b
Nidhi-Ranawat <84018561+Nidhi-Ranawat@users.noreply.github.com>
2021-09-04 16:16:21 +0530

    subName(alt)

--- prog1.cpp
+++ prog1.cpp
@@ -8,16 +8,19 @@ int main()
     int n,i;
     cout<<"Enter total number of subjects";
     cin>>n;
-    string arr[300];
+    string arr[300],course;
+    cin.ignore();
     for(i=0;i<n;i++)
     {
     cout<<"Enter the name of course "<<(i+1)<<":"<<"\n";
-    cin>>arr[i];
+    
+    getline(std::cin,arr[i]);
+    //cin>>arr[i];
     }
     cout<<"The name of subjects are";
     for(i=0;i<n;i++)
     {
-    cout<<(i+1)<<"Course name "<<"is"<< arr[i]<<"\n";
+    cout<<(i+1)<<"Course name "<<"is "<< arr[i]<<"\n";
     }
     getch();
 }
